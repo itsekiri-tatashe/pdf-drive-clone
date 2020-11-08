de57756c91e6a1858c9b7e6be52dc3cbea39c42d
Eyimofe Ayo Pinnick <eyimofe36@gmail.com>
2020-11-08 07:15:43 +0100

    Configured Core Application

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+	path('', views.HomeView.as_view(), name="home")
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Import Class Based View
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+	template_name = 'core/index.html'
+
+
